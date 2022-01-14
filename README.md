168caea4cc18851b0462a3db20eb237c437a79b9
Phil O'Sullivan <phil.osullivan@gmail.com>
2022-01-14 14:53:52 +0000

    select php version

--- README.md
+++ README.md
@@ -8,12 +8,14 @@ https://www.tal.org/tutorials/raspberrypi_php_apache
 
 Install MySQL:
 sudo apt install -y mariadb-server
-
+sudo mysql -u root "SET PASSWORD FOR 'root'@localhost = PASSWORD('1qaz2wsx');FLUSH PRIVILEGES;";
 
 https://serverfault.com/questions/783527/non-interactive-silent-install-of-mysql-5-7-on-ubuntu-16-04-lts
 
 Install Composer:
 https://lindevs.com/install-composer-on-raspberry-pi/
+
+bash ./install.sh
 
 TODO:
     sudo apt update -y && sudo apt upgrade -y;
@@ -22,11 +24,8 @@ TODO:
     Set Wifi.
     install Git.
     Localize keyboard & timezone.
-    Ask what version of PHP to install?
-    Ask what version of composer to install?
+    Add Coloring to console.
     Ask for Mysql Root PW.
-    Use .env file to personalize?
     Run clean up and delete this script/directory.
-    save log to home directory?
     test localhost?
     Restart after setup.
--- install.sh
+++ install.sh
@@ -1,216 +1,215 @@
-#!/usr/bin/bash
-    export DEBIAN_FRONTEND=noninteractive;
+#!/bin/bash
+	#export DEBIAN_FRONTEND=noninteractive;
 
-# Variables #   
-    LOG_DATE=`date +%m_%d_%Y`;
-    LOG_FILE="$HOME/logs/${LOG_DATE}.log";
-    CPU_INFO="/proc/cpuinfo";
-    MSG_NP="This device is most likley not a Raspberry Pi";
+# Variables #
+	HOSTNAME=$(hostname);
+	LOG_DATE=`date +%m_%d_%Y`;
+	LOG_FILE="$HOME/logs/${LOG_DATE}.log";
+	CPU_INFO="/proc/cpuinfo";
+	MSG_NP="This device is most likley not a Raspberry Pi";
 
+	# Supported PHP Versions #
+	# https://www.php.net/supported-versions.php
+	PHP_VERSIONS=(
+		"7.4"
+		"8.0"
+		"8.1"
+		"Quit"
+	);
 # Functions #
 
-    # Generic logging function #
-    log() {
-        # Make log file if it doesn't exist #
-        if [ ! -f "$LOG_FILE" ]; then
-            #sudo touch $LOG_FILE;
-            sudo mkdir -p "$HOME/logs/" && sudo touch "${LOG_FILE}";
-        fi
+	# Generic logging function #
+	log() {
+		# Make log file if it doesn't exist #
+		if [ ! -f "$LOG_FILE" ]; then
+			#sudo touch $LOG_FILE;
+			sudo mkdir -p "$HOME/logs/" && sudo touch "${LOG_FILE}";
+		fi
 
-        # Is the message empty, just add empty line to log #
-        if [ -z "$1" ] 
-        then 
-            MSG="";
-        else
-            TS=$(date -u);
-            # Timestamp, Log Message #
-            MSG="$TS - $1";
-        fi
+		# Is the message empty, just add empty line to log #
+		if [ -z "$1" ]
+		then
+			MSG="";
+		else
+			TS=$(date -u);
+			# Timestamp, Log Message #
+			MSG="$TS - $1";
+		fi
 
-        # Add a delay #
-        sleep 1s;
+		# Add a delay #
+		sleep 1s;
 
-        # Print message to log file #
-        echo "$MSG" | sudo tee -a $LOG_FILE;
-    }
+		# Print message to log file #
+		echo "$MSG" | sudo tee -a $LOG_FILE;
+	}
 
-    # Exit on fatal error #
-    error_exit() {
-        log "ERROR $1";
-        log "ERROR SCRIPT WILL NOW EXIT";
-        exit 1;
-    }
+	# Exit on fatal error #
+	error_exit() {
+		log "ERROR $1";
+		log "ERROR SCRIPT WILL NOW EXIT";
+		exit 1;
+	}
 
-    # Start Script #
-    log "INFO Script Started";
+	# Start Script #
+	log "INFO Script Started";
 
-    # Try and ping the  outside world #
-    wget -q --spider http://google.com
+	# Try and ping the  outside world #
+	wget -q --spider http://google.com
 
-    # Eval Internet access #
-    if [ $? -eq 0 ]; then
-        log "INFO INTERNET ACCESS: SUCCESS";
-    else
-        error_exit "NO INTERNET ACCESS, PLEASE SETUP AN INTERNET CONNECTION";
-    fi
+	# Eval Internet access #
+	if [ $? -eq 0 ]; then
+		log "INFO INTERNET ACCESS: SUCCESS";
+	else
+		error_exit "NO INTERNET ACCESS, PLEASE SETUP AN INTERNET CONNECTION";
+	fi
 
-    # Load options from .env file #
-    if [ -f .env ]
-    then
-        log "INFO ENV_FILE: FOUND";
-        export $(cat .env | grep -v '#' | awk '/=/ {print $1}');
+		# Check all is good #
+	log "INFO Gathering System config";
 
-        log "INFO ENV_PHP_VER: ${ENV_PHP_VER}";
-        log "INFO ENV_MYSQL_ROOT_PASSWORD: ${ENV_MYSQL_ROOT_PASSWORD}";
-        log "INFO ENV_MYSQL_USER_NAME: ${ENV_MYSQL_USER_NAME}";
-        log "INFO ENV_MYSQL_USER_PW: ${ENV_MYSQL_USER_PW}";
-        log "INFO ENV_HOSTNAME: ${ENV_HOSTNAME}";
+	# Select PHP Version to Install. #
+	PS3="Select the version of PHP to install: "
+	select PHP_VERSION in ${PHP_VERSIONS[@]}
+	do
+		# Quit #
+		if [ $PHP_VERSION == "Quit" ]; then
+			error_exit "NO PHP VERSION SELECTED";
+		fi
+		# Invalid option selected #
+		if [[ ! " ${PHP_VERSIONS[*]} " =~ " ${PHP_VERSION} " ]]; then
+			error_exit "INVALID PHP VERSION SELECTED";
+		fi
+		echo "Selected PHP VERSION: ${PHP_VERSION}";
+		break
+	done
 
-        # Eval ENV Variables. #
-        
-    else 
-        error_exit "ENV_FILE: NOT FOUND";
-    fi
+	# System Info #
+	if test -f "$CPU_INFO"; then
+		# Set Raspberry Pi specific variables #
+		SERIAL=$(sed -n 's/^Serial\s*: 0*//p' /proc/cpuinfo);
+		MODEL=$(sed -n 's/^Model\s*: 0*//p' /proc/cpuinfo);
+		HARDWARE=$(sed -n 's/^Hardware\s*: 0*//p' /proc/cpuinfo);
+		REVISION=$(sed -n 's/^Revision\s*: 0*//p' /proc/cpuinfo);
 
-    # System Info #
-    if test -f "$CPU_INFO"; then
-        # Set Raspberry Pi specific variables #
-        SERIAL=$(sed -n 's/^Serial\s*: 0*//p' /proc/cpuinfo);
-        MODEL=$(sed -n 's/^Model\s*: 0*//p' /proc/cpuinfo);
-        HARDWARE=$(sed -n 's/^Hardware\s*: 0*//p' /proc/cpuinfo);
-        REVISION=$(sed -n 's/^Revision\s*: 0*//p' /proc/cpuinfo);
-        HOSTNAME=$(hostname);
 
-        log "INFO SERIAL Number: ${SERIAL}";
-        log "INFO MODEL Number: ${MODEL}";
-        log "INFO HARDWARE: ${HARDWARE}";
-        log "INFO REVISION: ${REVISION}";
-        log "INFO HOSTNAME: ${HOSTNAME}";
-    else 
-        error_exit "${MSG_NP}";
-    fi
+		log "INFO SERIAL Number: ${SERIAL}";
+		log "INFO MODEL Number: ${MODEL}";
+		log "INFO HARDWARE: ${HARDWARE}";
+		log "INFO REVISION: ${REVISION}";
+		log "INFO HOSTNAME: ${HOSTNAME}";
+		log "INFO PHP_VERSION: ${PHP_VERSION}";
+	else
+		error_exit "${MSG_NP}";
+	fi
 
-    # Install Apps/Software #
+	# Install Apps/Software #
 
-    # EXPECT #
-    EXPECT=$(expect -v);
-    if [ -z "${EXPECT}" ]
-    then
-       log "ERROR EXPECT is not installed";
-       log "INFO Setting EXPECT";
-       sudo apt update;
-       sudo apt install -y expect;
-    else
-        log "INFO EXPECT: Installed";
-    fi
+	# PHP Repository #
+	PHP=$(php -r 'echo PHP_VERSION;');
+	if [ -z "${PHP}" ]
+	then
+	   log "ERROR PHP is not installed";
+	   log "INFO Setting PHP Repository";
 
-    # PHP Repository #
-    PHP=$(php -r 'echo PHP_VERSION;');
-    if [ -z "${PHP}" ]
-    then
-       log "ERROR PHP is not installed";
-       log "INFO Setting PHP Repository";
+	   wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
 
-       wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+	   echo "deb https://packages.sury.org/php/ buster main" | sudo tee /etc/apt/sources.list.d/php7.list
 
-       echo "deb https://packages.sury.org/php/ buster main" | sudo tee /etc/apt/sources.list.d/php7.list
+	   sudo apt update
+	else
+		log "INFO PHP Repository: ${PHP} Setup";
+	fi
 
-       sudo apt update
-    else
-        log "INFO PHP Repository: ${PHP} Setup";
-    fi
+	# APACHE #
+	APACHE=$(apache2 -v);
+	if [ -z "${APACHE}" ]
+	then
+	   log "ERROR APACHE is not installed";
+	   log "INFO Installing APACHE";
+	   sudo apt install -y apache2 libapache2-mod-fcgid;
+	else
+		log "INFO APACHE: ${APACHE} Installed";
+	fi
 
-    # APACHE #
-    APACHE=$(apache2 -v);
-    if [ -z "${APACHE}" ]
-    then
-       log "ERROR APACHE is not installed";
-       log "INFO Installing APACHE";
-       sudo apt install -y apache2 libapache2-mod-fcgid;
-    else
-        log "INFO APACHE: ${APACHE} Installed";
-    fi
+	# PHP #
+	PHP=$(php -r 'echo PHP_VERSION;');
+	if [ -z "${PHP}" ]
+	then
+	   log "ERROR PHP is not installed";
+	   log "INFO Installing PHP";
 
-    # PHP #
-    PHP=$(php -r 'echo PHP_VERSION;');
-    if [ -z "${PHP}" ]
-    then
-       log "ERROR PHP is not installed";
-       log "INFO Installing PHP";
-       
-       sudo apt update;
+	   sudo apt update;
 
-       sudo apt install -y php7.3-cli php7.3-fpm \
-       php7.3-opcache php7.3-curl php7.3-mbstring \
-       php7.3-pgsql php7.3-zip php7.3-xml php7.3-gd;
+	   sudo apt install -y php7.3-cli php7.3-fpm \
+	   php7.3-opcache php7.3-curl php7.3-mbstring \
+	   php7.3-pgsql php7.3-zip php7.3-xml php7.3-gd;
 
-       log "INFO Enabling PHP-FPM";
+	   log "INFO Enabling PHP-FPM";
 
-       sudo a2enmod proxy_fcgi;
-       sudo a2enconf php7.3-fpm;
+	   sudo a2enmod proxy_fcgi;
+	   sudo a2enconf php7.3-fpm;
 
-       log "INFO Reloading Apache2";
-       sudo systemctl reload apache2;
+	   log "INFO Reloading Apache2";
+	   sudo systemctl reload apache2;
 
-    else
-        log "INFO PHP: ${PHP} Installed";
-    fi
+	else
+		log "INFO PHP: ${PHP} Installed";
+	fi
 
-    # Composer #
-    COMPOSER=$(composer --version);
-    if [ -z "${COMPOSER}" ]
-    then
-       log "ERROR COMPOSER is not installed";
-       log "INFO Installing Composer";
+	# Composer #
+	COMPOSER=$(composer --version);
+	if [ -z "${COMPOSER}" ]
+	then
+	   log "ERROR COMPOSER is not installed";
+	   log "INFO Installing Composer";
 
-       wget -O composer-setup.php https://getcomposer.org/installer
+	   wget -O composer-setup.php https://getcomposer.org/installer
 
-       sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+	   sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-       log "INFO Removeing Composer setup files";
-       rm -rf composer-setup.php
-    else
-        log "INFO COMPOSER: ${COMPOSER} Installed";
-    fi
+	   log "INFO Removeing Composer setup files";
+	   rm -rf composer-setup.php
+	else
+		log "INFO COMPOSER: ${COMPOSER} Installed";
+	fi
 
-    # MYSQL #
-    DB=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }');
-    if [ -z "${DB}" ]
-    then
-       log "ERROR MySQL is not installed";
-       log "INFO Installing MySQL";
-       sudo apt update;
-       sudo apt install -y mariadb-server;
+	# MYSQL #
+	DB=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }');
+	if [ -z "${DB}" ]
+	then
+	   log "ERROR MySQL is not installed";
+	   log "INFO Installing MySQL";
+	   sudo apt update;
+	   sudo apt install -y mariadb-server;
 
-        log "INFO Securing MySQL Install";
+		log "INFO Securing MySQL Install";
+		log "INFO Watch for Prompts";
 
-        log "INFO MySQL Changing root password";
-        sudo mysql -u root "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ENV_MYSQL_ROOT_PASSWORD}');FLUSH PRIVILEGES;";
-        
-        # log "INFO MySQL Deleting Test users";
-        # sudo mysql -uroot -p${ENV_MYSQL_ROOT_PASSWORD} "DELETE FROM mysql.user WHERE User='';"
+		sleep 2s;
 
-        # sudo mysql -uroot -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-        
-        # log "INFO MySQL Deleting Test Databases";
-        # sudo mysql -uroot -p${ENV_MYSQL_ROOT_PASSWORD} "DROP DATABASE test;DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';"
+		mysql_secure_installation
 
-        # log "INFO MySQL Add User";
-        # sudo mysql -u root -p${ENV_MYSQL_ROOT_PASSWORD} -e "CREATE USER '${ENV_MYSQL_USER_NAME}'@'localhost' IDENTIFIED BY '${ENV_MYSQL_USER_PW}';GRANT ALL PRIVILEGES ON *.* TO '${ENV_MYSQL_USER_NAME}'@'localhost';FLUSH PRIVILEGES;"
+		log "INFO MYSQL Installation Complete";
 
-        log "INFO MYSQL Installation Complete";
+	else
+		log "INFO MySQL: ${DB}";
+	fi
 
-    else
-        log "INFO MySQL: ${DB}";
-    fi
-    
-    # Check all is good #
-    log "INFO Running System Check";
 
-    # Cleanup #
-    log "INFO Running Cleanup";
-    
-    # End Script #
-    log "INFO Script Complete";
+
+
+
+
+
+
+
+
+	# Check all is good #
+	log "INFO Running System Check";
+
+	# Cleanup #
+	log "INFO Running Cleanup";
+
+	# End Script #
+	log "INFO Script Complete";
 
 exit 0;
